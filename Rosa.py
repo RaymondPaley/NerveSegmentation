b22be2e19d10789e51eef141b37c855842b322b9
Neelesh Tiruviluamala <Neelesh.Tiruviluamala@gmail.com>
2016-07-29 11:33:23 -0700

    Individual Files

--- Rosa.py
+++ Rosa.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Fri Jul 29 11:18:38 2016
+
+@author: Abecedarian
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Fri Jul 15 23:25:42 2016
 
 @author: Abecedarian
@@ -22,7 +29,6 @@ import numpy as np
 import os
 import sys
 import csv
-#import matplotlib.pyplot as plt
 
 #Set up Test Data
 test_folder = '/Images/test'
@@ -34,7 +40,7 @@ test_num = len(dir_list_test)
 
 
 
-X_test = np.empty((100, 1, 42,58))
+X_test = np.empty((1, 1, 42,58))
 
 #Create mechanism for working through minibatches
 def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
@@ -60,19 +66,19 @@ network = lasagne.layers.InputLayer((None, 1, 42, 58), input_var)
 network = lasagne.layers.Conv2DLayer(network, 32, (5, 5),
                                      nonlinearity=leaky_rectify)
 
-network = lasagne.layers.Pool2DLayer(network, (2, 2), mode='max')
+network = lasagne.layers.Pool2DLayer(network, (3, 3), mode='max')
 
-network = lasagne.layers.Conv2DLayer(network, 64, (5, 5),
+network = lasagne.layers.Conv2DLayer(network, 64, (5, 5),stride = 2,
                                      nonlinearity=leaky_rectify)
                                      
-#network = lasagne.layers.Pool2DLayer(network, (3, 3), mode='max')
+network = lasagne.layers.Pool2DLayer(network, (3, 3), mode='max')
                           
 network = lasagne.layers.DenseLayer(lasagne.layers.dropout(network, 0.5),
                                     128, nonlinearity=leaky_rectify,
                                     W=lasagne.init.Orthogonal())
                                     
 network = lasagne.layers.DenseLayer(lasagne.layers.dropout(network, 0.5),
-                                    2, nonlinearity=softmax)
+                                    10, nonlinearity=softmax)
 
 # create loss function
 prediction = lasagne.layers.get_output(network)
@@ -89,11 +95,11 @@ updates = lasagne.updates.nesterov_momentum(loss, params, learning_rate=0.0001,
 train_fn = theano.function([input_var, target_var], loss, updates=updates, allow_input_downcast = True)
 
 # load previously obtained params
-
-with np.load('/Images/model_random_chunks_binaryBlocks.npz') as f:
-     param_values = [f['arr_%d' % i] for i in range(len(f.files))]
-
-lasagne.layers.set_all_param_values(network, param_values)
+#
+#with np.load('/Images/model_random_chunks_3.npz') as f:
+#     param_values = [f['arr_%d' % i] for i in range(len(f.files))]
+#
+#lasagne.layers.set_all_param_values(network, param_values)
 
 #Set up Training Data
 train_folder = '/Images/train'
@@ -105,38 +111,25 @@ dir_list = [x for x in dir_list if not('mask' in x)]
 #Train in Chunks
 train_num = 200
 
-X_train = np.empty((train_num*100, 1, 42,58))
-y_train = np.empty(train_num*100)
+X_train = np.empty((train_num, 1, 420,580))
+y_train = np.empty(train_num)
 
 with open('/Images/errors.csv','w') as csvfile:
     error = csv.writer(csvfile, delimiter=',')
     error.writerow(['Train Error'])    
     
-    for passNumber in range(1):
+    for passNumber in range(5):
         np.random.seed(passNumber)       
         shuffled = np.random.choice(range(len(dir_list)), train_num, replace = False)
           
-        for i in range(train_num): 
-            for j in range(100):
-                k = j % 10
-                l = (j - k)/10
-                X_train[100*i+j,0] = misc.imread(train_folder+os.sep+dir_list[shuffled[i]])[42*k:(42*k+42),58*l:(58*l+58)]
-                y_train[100*i+j] = int(np.sum(misc.imread(train_folder + os.sep+dir_list[shuffled[i]].split('.')[0] + '_mask.tif')[42*k:(42*k+42),58*l:(58*l+58)])/(4.2*58*255))
-                
-                y_train[100*i+j] = int(y_train[100*i+j] > 0)                
-                                
-                ###Testing this code
-#                temp = np.zeros((420,580))                
-#                for m in range(100):
-#                    k = m % 10
-#                    l = (m-k)/10
-#                    temp[42*k:(42*k+42),58*l:(58*l+58)] = (y_train[i*100+m])*25.5
-#                plt.imshow(temp)
-#                plt.imshow(misc.imread(train_folder + os.sep+dir_list[shuffled[i]].split('.')[0] + '_mask.tif'))
-#       
+        for i in range(train_num):        
+            X_train[i,0] = misc.imread(train_folder+os.sep+dir_list[shuffled[i]])
+            y_train[i] = int(np.sum(misc.imread(train_folder + os.sep+dir_list[shuffled[i]].split('.')[0] + '_mask.tif')) > 0)
+        
+        
         # train network
         num_epochs = 1
-        batch_size = 200
+        batch_size = 2
         # We iterate over epochs:
         for epoch in range(num_epochs):
             # In each epoch, we do a full pass over the training data:
@@ -156,59 +149,23 @@ with open('/Images/errors.csv','w') as csvfile:
         ## use trained network for predictions
         test_prediction = lasagne.layers.get_output(network, deterministic=True)
         
-        np.savez('/Images/model_random_chunks_binaryBlocks.npz', *lasagne.layers.get_all_param_values(network))
+        np.savez('/Images/model_random_chunks_3.npz', *lasagne.layers.get_all_param_values(network))
         
-        predict_fn = theano.function([input_var], T.argmax(test_prediction, axis=1),allow_input_downcast = True)
-        
-        ## Print out something useful
-        
-        Neel_Error = sum(abs(predict_fn(X_train[0:1000]) - y_train[0:1000]))
-        print("Neel_Error for 10 images: %r" %Neel_Error)
+        predict_fn_show = theano.function([input_var], T.argmax(test_prediction, axis=1),allow_input_downcast = True)
+        predict_fn = theano.function([input_var], test_prediction[:,1],allow_input_downcast = True) 
+    
+        print("Predicted class for first train input: %r" % predict_fn_show(X_train[0:10]))
+        print(y_train[0:10])
+        dif = predict_fn_show(X_train[0:train_num])-y_train[0:train_num]
+        print(sum(dif != 0))
         error.writerow([train_err / train_batches])
 
 ## Saving 
-with open('/Images/Block_Profiles.csv','w') as csvfile:
+with open('/Images/Predicted_Classes.csv','w') as csvfile:
     imagesub = csv.writer(csvfile, delimiter=',')
-    imagesub.writerow(['img','Block_Num','Block_Profile'])
+    imagesub.writerow(['img','Prediction'])
     for i in range(test_num):
         imageNum = dir_list_test[i].split('.')[0]
-        for j in range(100):
-            k = j % 10
-            l = (j - k)/10
-            X_test[j,0] = misc.imread(test_folder+os.sep+dir_list_test[i])[42*k:(42*k+42),58*l:(58*l+58)]        
-            imagesub.writerow([imageNum, j, predict_fn(X_test[j:j+1])[0]])
-
-##
-
-######## Making a submission
-#blockInfo = np.zeros((test_num*100,3))
-#
-##with open('/Users/Abecedarian/Desktop/Block_Profiles.csv', 'rb') as f:
-#with open('/Images/Block_Profiles.csv', 'rb') as f:
-#    reader = csv.reader(f)
-#    l = 0    
-#    for row in reader:
-#        if l != 0:
-#            blockInfo[l-1] = row
-#        l = l+1
-#
-### Looking at Some Images
-#
-#i = 3
-#temp = np.zeros((420,580))                
-#for m in range(100):
-#    k = m % 10
-#    l = (m-k)/10
-#    temp[42*k:(42*k+42),58*l:(58*l+58)] = (blockInfo[i*100+m])*25.5
-#plt.imshow(temp)
-#
-#
-#with open('/Images/Blocks_Submission.csv','w', newline='') as csvfile:
-#    imagesub = csv.writer(csvfile)
-#    imagesub.writerow(['img','pixels'])
-#    for im in testImages:
-#        imageNum = int(im.split(os.sep)[6].split('.')[0])        
-#        image = misc.imread(im)
-#        image = misc.imresize(image, (42,58))
-#        
-#        imagesub.writerow([imageNum,'1 243600'])
+        X_test[0,0] = misc.imread(test_folder+os.sep+dir_list_test[i])        
+                
+        imagesub.writerow([imageNum,predict_fn(X_test[0:1])[0]])
